38aa9686f7e4bd60d15b41eba10a4dc56793fb0a
PhamVanLuong02 <phamluong.251102@gmail.com>
2023-12-26 10:39:29 +0700

    fix sth

---
+++ FrontEnd/src/app/components/client/checkout/checkout.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { faBars, faHeart, faPhone, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import { MessageService } from 'primeng/api';
+import { Order } from 'src/app/_class/order';
+import { OrderDetail } from 'src/app/_class/order-detail';
+
+import { CartService } from 'src/app/_service/cart.service';
+import { OrderService } from 'src/app/_service/order.service';
+import { StorageService } from 'src/app/_service/storage.service';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css'],
+  providers: [MessageService]
+
+})
+export class CheckoutComponent implements OnInit {
+  heart = faHeart;
+  bag = faShoppingBag;
+  phone = faPhone;
+  bars = faBars;
+  showDepartment = false;
+  order = new Order();
+  listOrderDetail: any[] = [];
+  username !: string;
+
+  orderForm: any = {
+    firstname: null,
+    lastname: null,
+    country: null,
+    addrest: null,
+    town: null,
+    state: null,
+    postCode: null,
+    email: null,
+    phone: null,
+    note: null
+  }
+
+  constructor(public cartService: CartService, private orderService: OrderService, private storageService: StorageService, private messageService: MessageService) {
+
+  }
+  ngOnInit(): void {
+    this.username = this.storageService.getUser().username;
+    this.cartService.getItems();
+    console.log(this.username);
+  }
+
+  showDepartmentClick() {
+    this.showDepartment = !this.showDepartment;
+  }
+
+  placeOrder() {
+    this.cartService.items.forEach(res => {
+      let orderDetail: OrderDetail = new OrderDetail;
+      orderDetail.name = res.name;
+      orderDetail.price = res.price;
+      orderDetail.quantity = res.quantity;
+      orderDetail.subTotal = res.subTotal;
+      this.listOrderDetail.push(orderDetail);
+      this.showSuccess("Đặt hàng thành công")
+    })
+
+    const { firstname, lastname, country, address, town, state, postCcode, phone, email, note } = this.orderForm;
+    this.orderService.placeOrder(firstname, lastname, country, address, town, state, postCcode, phone, email, note, this.listOrderDetail, this.username).subscribe({
+      next: res => {
+        this.cartService.clearCart();
+      }, error: err => {
+        console.log(err);
+      }
+    })
+
+  }
+  showSuccess(text: string) {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: text });
+  }
+  showError(text: string) {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: text });
+  }
+
+  showWarn(text: string) {
+    this.messageService.add({ severity: 'warn', summary: 'Warn', detail: text });
+  }
+
+}
